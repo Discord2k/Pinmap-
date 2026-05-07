13cac54e577d9d9be23834f76f1f7aeab6180b65
Discord2k <107446920+Discord2k@users.noreply.github.com>
2026-05-06 22:34:16 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "pinmap-v169";
+var CACHE_NAME = "pinmap-v171";
 var TILE_CACHE = "pinmap-tiles-v1";
 var MAX_TILES = 1000;
 var APP_SHELL = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
