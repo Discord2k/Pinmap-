8e73ee92fbfec2a0c7a94f905653555eec0d7124
Discord2k <107446920+Discord2k@users.noreply.github.com>
2026-05-11 19:44:37 -0400

    fix: bump SW cache to v215 to clear stale vite-test manifest

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "pinmap-v214";
+var CACHE_NAME = "pinmap-v215";
 var TILE_CACHE = "pinmap-tiles-v2";
 var MAX_TILES = 10000;
 var APP_SHELL = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
